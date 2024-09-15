9318409d726ad5c3a92a730ded47d86d3b52fc08
Raivis Petrovskis <rapet80@gmail.com>
2024-09-15 19:39:42 +0100

    remove files

--- get_id.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def generate_customer_id():
-    # Get the current timestamp in microseconds
-    timestamp = int(time.time() * 100)
-    return str(timestamp)
-
-# Generate and print the customer ID
-print(generate_customer_id())
